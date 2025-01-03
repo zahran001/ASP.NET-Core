50275b031d3853876af3484fe352e38c2ca01eb6
Zahran Yahia Khan <114562123+zahran001@users.noreply.github.com>
2025-01-02 22:31:25 -0800

    added xml support

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -2,7 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddControllers(option =>
+{
+    option.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,3 +26,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// In an HTTP GET request, the Accept header indicates the type(s) of content the client (e.g., a browser or API client) is willing to receive in response to the request.
